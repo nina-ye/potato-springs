61d47fb5f302db50e175553033902fc8baca955c
Nina Ye <nye@twilio.com>
2020-10-14 16:26:39 -0700

    Add basic storybook examples

---
+++ src/components/CryingPotato/cryingPotato.stories.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { Meta, Story } from "@storybook/react";
+import { CryingPotato } from "./index";
+
+export default {
+  title: "Potato Spring Animations/Crying Potato",
+  component: CryingPotato,
+  decorators: [
+    Story => (
+      <div
+        style={{
+          width: "100vw",
+          height: "50vw",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <Story />
+      </div>
+    ),
+  ],
+} as Meta;
+
+const Template: Story = args => <CryingPotato {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {};
---
+++ src/components/StaringPotato/staringPotato.stories.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { Meta, Story } from "@storybook/react";
+import { StaringPotato } from "./index";
+
+export default {
+  title: "Potato Spring Animations/Staring Potato",
+  component: StaringPotato,
+  decorators: [
+    Story => (
+      <div
+        style={{
+          width: "100vw",
+          height: "50vw",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <Story />
+      </div>
+    ),
+  ],
+} as Meta;
+
+const Template: Story = args => <StaringPotato {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {};
