ebf94f4367a585528c792d55e3a623ed40eb993d
geleazar1000111 <geraldine@MacBook-Pro.local>
2020-10-22 20:48:09 -0700

    added garftato

---
+++ src/components/GarfTato/index.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { useSpring, animated } from "react-spring";
+import { useDrag } from 'react-use-gesture';
+import GarfTato from "../../assets/garftato.png";
+import "./index.scss";
+
+export const GarfPotato: React.FC = () => {
+    const [{ xy }, set] = useSpring(() => ({ xy: [0, 0] }))
+    const bind = useDrag(({ down, movement }) => {
+        set({ xy: down ? movement : [0, 0] })
+      })
+    
+    return (
+        <animated.img
+        {...bind()}
+        src={GarfTato}
+        alt="garf potato"
+        style={{
+            transform: xy.interpolate((x, y) => `translate3d(${x}px, ${y}px, 0)`),
+          }}
+        />
+      );
+};
