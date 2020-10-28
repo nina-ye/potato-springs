0751cfe49a47f8e64387c7c7b0a8e7166182d710
Nina Ye <nye@twilio.com>
2020-10-28 14:06:35 -0700

    Add build

---
+++ docs/precache-manifest.d3d185340a26876b3e7ccb3051949a3f.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d623cebd950aea69c2000274b9c6c6e1",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.ac43e1ae4c182a9c85b4.bundle.js"
+  },
+  {
+    "url": "runtime~main.ac43e1ae4c182a9c85b4.bundle.js"
+  },
+  {
+    "revision": "45f516cf7145c71ddaad",
+    "url": "static/css/main.243a200d.chunk.css"
+  },
+  {
+    "revision": "7e74340ef56e73936660be4451383703",
+    "url": "static/media/cute-potato-base.7e74340e.png"
+  },
+  {
+    "revision": "08c14002829e57ea63f429a6b19f7513",
+    "url": "static/media/evil-potato.08c14002.png"
+  },
+  {
+    "revision": "530955bbe0990a8713d80da750a6e66d",
+    "url": "static/media/garftato.530955bb.png"
+  },
+  {
+    "revision": "e6f2f8d405b84f179a9075e26b37f5f3",
+    "url": "static/media/happy-potato-2.e6f2f8d4.png"
+  },
+  {
+    "revision": "05f957cf7a4755d9cc2abfb634f6b06f",
+    "url": "static/media/sad-potato.05f957cf.png"
+  },
+  {
+    "revision": "71f4df4aa1c1c783fe55fd2dda386b65",
+    "url": "static/media/speechless-potato.71f4df4a.png"
+  },
+  {
+    "url": "vendors~main.ac43e1ae4c182a9c85b4.bundle.js"
+  },
+  {
+    "url": "vendors~main.ac43e1ae4c182a9c85b4.bundle.js.LICENSE.txt"
+  }
+]);
