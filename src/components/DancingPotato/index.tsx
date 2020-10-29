f97c94cf5a41cf03b4e234e93294103b3ff00751
Nina Ye <nye@twilio.com>
2020-10-28 21:51:05 -0700

    Ts errors ):

--- src/components/DancingPotato/index.tsx
+++ src/components/DancingPotato/index.tsx
@@ -13,8 +13,8 @@ export const DancingPotato: React.FC<DancingPotatoProps> = ({
   const [didShake, setShake] = useState(false);
   const { x } = useSpring({
     from: { x: 0 },
-    to: { x: didShake ? 1 : 0 }, 
-    config: {mass: 1, tension: 500, friction: friction}
+    to: { x: didShake ? 1 : 0 },
+    config: { mass: 1, tension: 500, friction: friction },
   });
 
   return (
@@ -23,15 +23,15 @@ export const DancingPotato: React.FC<DancingPotatoProps> = ({
         src={CoolPotato}
         alt="cool potato"
         draggable={false}
-        // @ts-ignore
         style={{
-          transform : x
+          transform: x
             .interpolate({
-              range: [0, .25, .50, .75, 1],
-              output: [0, 10, 20, 30, 40]
+              range: [0, 0.25, 0.5, 0.75, 1],
+              output: [0, 10, 20, 30, 40],
             })
-            .interpolate(x => `translate3d(${x}px, 0px, 0px)`)
-          }}
+            // @ts-ignore
+            .interpolate(x => `translate3d(${x}px, 0px, 0px)`),
+        }}
         onClick={() => setShake(!didShake)}
       />
     </div>
